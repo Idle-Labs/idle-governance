92f00d34ae38df9550092ed8a29641c9bf9ea289
Andrea Franz <andrea@gravityblast.com>
2021-09-08 14:16:49 +0200

    update price oracle test

--- migrations/6_deploy_price_oraclev3.js
+++ migrations/6_deploy_price_oraclev3.js
@@ -13,16 +13,9 @@ module.exports = async function (deployer, network, accounts) {
   console.log('PriceOracleV3:', oracle.address);
   await oracle.transferOwnership(timelock, {from: creator});
 
-  const tokensUSD = {
+  const tokens = {
     weth: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
     comp: "0xc00e94cb662c3520282e6f5717214004a7f26888",
-    wbtc: "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599",
-    dai: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
-    stkAave: "0x4da27a545c0c5B758a6BA100e3a049001de870f5",
-    fei: "0x956f47f50a910163d8bf957cf5846d573e7f87ca",
-  }
-
-  const tokensETH = {
     wbtc: "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599",
     dai: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
     susd: "0x57ab1ec28d129707052df4df418d58a2d46d5f51",
@@ -34,13 +27,11 @@ module.exports = async function (deployer, network, accounts) {
     rai: "0x03ab458634910aad20ef5f1c8ee96f1d6ac54919",
   }
 
-  for (const tokenName in tokensUSD) {
-    const priceUSD = await oracle.getPriceUSD(tokensUSD[tokenName]);
+  for (const tokenName in tokens) {
+    const priceUSD = await oracle.getPriceUSD(tokens[tokenName]);
     console.log(`${tokenName} priceUSD: ${priceUSD.toString()}`);
-  }
-
-  for (const tokenName in tokensETH) {
-    const priceETH = await oracle.getPriceETH(tokensETH[tokenName]);
+    const priceETH = await oracle.getPriceETH(tokens[tokenName]);
     console.log(`${tokenName} priceETH: ${priceETH.toString()}`);
+    console.log();
   }
 };
